32c893f914b4f74b537d6290e32f7c197af8e37e
Renan-Marquet <224832176+Renan-Marquet@users.noreply.github.com>
2025-12-13 12:18:39 -0300

    inicio projeto

---
+++ ProjetoVG/core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+#from django.http import HttpResponse
+
+def home(request):
+    #return HttpResponse("<h1> Bem Vindos à Venture Gotchi!</h1>")
+    context={
+        'nome_usuario':'Gotcher',
+        'atividades':['Desafio_01','Desafio_02','Desafio_03']
+    }
+    return render(request,'home.html',context)
+
+# Create your views here.
