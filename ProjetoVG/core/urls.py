b5b7331b415b9e035073ba365687d77dae0e93ad
VianaMiguel <224689790+VianaMiguel@users.noreply.github.com>
2025-12-14 16:26:20 -0300

    Implementa tela de dashboard e ajustes em views/urls

--- ProjetoVG/core/urls.py
+++ ProjetoVG/core/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name = 'home'),
+    path("", views.home, name="home"),
+    path(
+        "dashboard/", views.dashboard, name="dashboard"
+    ),  # ligação de url para a página dashboard
 ]
