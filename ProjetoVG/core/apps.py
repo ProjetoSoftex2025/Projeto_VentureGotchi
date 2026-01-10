00aa922d3853348961e30447af0ea7b1e8272180
VianaMiguel <224689790+VianaMiguel@users.noreply.github.com>
2026-01-09 23:38:59 -0300

    docs: adiciona comentário no AppConfig

--- ProjetoVG/core/apps.py
+++ ProjetoVG/core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+# Configuração principal do app Core, responsável pelo registro do aplicativo e definição de comportamentos padrão dentro do projeto Django.
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
