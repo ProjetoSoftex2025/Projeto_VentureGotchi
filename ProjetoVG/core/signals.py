9e5f9e7a65c8fe9833e07911f0d32668b36d2c2e
VianaMiguel <224689790+VianaMiguel@users.noreply.github.com>
2026-01-09 23:43:30 -0300

    feat: add signals para automatizar ações do core

---
+++ ProjetoVG/core/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver 
+from django.contrib.auth.models import User 
+from .models import Progresso 
+# Signal responsável por criar automaticamente o Progresso sempre que um novo usuário é criado no sistema.
+# Evita erros e garante consistência dos dados.
+
+@receiver(post_save, sender=User)
+
+def criar_progresso_usuario(sender, instance, created, **kwargs): 
+    if created:
+        Progresso.objects.create(usuario=instance) 
+    else: 
+        # garante consistência se você mudar a lógica de salvamento do usuário
+        progresso = getattr(instance, "progresso", None) 
+        if progresso:
+            progresso.atualizar_nivel_e_meta()
