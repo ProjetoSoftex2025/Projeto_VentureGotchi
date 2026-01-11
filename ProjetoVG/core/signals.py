10cc296fb150edfe74fdb17dd2137de5dc98a6ba
VianaMiguel <224689790+VianaMiguel@users.noreply.github.com>
2026-01-11 00:11:25 -0300

    fix: conserta o erro do progresso não aparecer

--- ProjetoVG/core/signals.py
+++ ProjetoVG/core/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver 
 from django.contrib.auth.models import User 
-from .models import Progresso 
+from .models import Progresso, UserProgress
 # Signal responsável por criar automaticamente o Progresso sempre que um novo usuário é criado no sistema.
 # Evita erros e garante consistência dos dados.
 
@@ -13,5 +13,6 @@ def criar_progresso_usuario(sender, instance, created, **kwargs):
     else: 
         # garante consistência se você mudar a lógica de salvamento do usuário
         progresso = getattr(instance, "progresso", None) 
-        if progresso:
+        if progresso and hasattr(progresso, "atualizar_nivel_e_meta"):
             progresso.atualizar_nivel_e_meta()
+            
