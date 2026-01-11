70f0a3f4c8d02555dacb8666bcf20392bee25ba8
VianaMiguel <224689790+VianaMiguel@users.noreply.github.com>
2026-01-11 00:15:40 -0300

    feat:  add acionamento do app

--- ProjetoVG/core/apps.py
+++ ProjetoVG/core/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self): # Método chamado quando o aplicativo está pronto.
+        import core.signals
