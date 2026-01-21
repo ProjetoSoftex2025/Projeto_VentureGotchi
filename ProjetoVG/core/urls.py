1baded3b0c79ccf18cb8e533f7c08e3487a9821e
Lussandro <lussandrorj2@hotmail.com>
2026-01-21 00:47:41 -0300

    fix(core): corrigir rotas de dashboard, login e cadastro

--- ProjetoVG/core/urls.py
+++ ProjetoVG/core/urls.py
@@ -1,21 +1,12 @@
 from django.urls import path
 from . import views
-from .views import CustomLoginView
-from django.contrib.auth.views import LogoutView
 
-#app_name = "core"
+app_name = "core"
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path(
-        "dashboard/", views.dashboard, name="dashboard"
-    ),  # ligação de url para a página dashboard
-    path(
-        "progresso/", views.progresso, name="progresso"
-    ),  # ligação de url para a página progresso
-     path('login/', CustomLoginView.as_view(), name='login'), # ligação de url para a página de login
-     path('register/', views.register, name='register'), # ligação de url para a página de cadastro
-     path('logout/', LogoutView.as_view(), name='logout'), # ligação de url para a página de logout
-     
-
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("progresso/", views.progresso, name="progresso"),
+    path("register/", views.register, name="register"),
+    path("login/", views.CustomLoginView.as_view(), name="login"),
 ]
